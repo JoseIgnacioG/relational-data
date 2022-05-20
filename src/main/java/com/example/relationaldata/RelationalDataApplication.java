fb5e98ad66639f2bbb2d631282bd8226bd8fcd10
Jose Ignacio G <jogg@gft.com>
2022-05-20 20:18:06 +0200

    Creando el programa de "Accessing Relational Data using JDBC with Spring"

---
+++ src/main/java/com/example/relationaldata/RelationalDataApplication.java
@@ -0,0 +1,14 @@
+// https://spring.io/guides/gs/relational-data-access/
+package com.example.relationaldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationalDataApplication.class, args);
+    }
+
+}
